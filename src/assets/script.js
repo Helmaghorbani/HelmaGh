1efdde769ba81a2766a1a8e99391706dd538c2a7
Helma Ghorbani <helmaghqorbani21@gmail.com>
2025-04-08 09:52:19 +0330

    Add hamburger menu to navbar

--- src/assets/script.js
+++ src/assets/script.js
@@ -7,4 +7,14 @@ window.onload = function() {
         });
       });
     }
-  };
+  
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', function () {
+      navLinks.classList.toggle('active');
+      menuToggle.classList.toggle('open');
+    });
+  }
+};
